dc2b05f3281091bb6ab0f08206980a9b021a2dad
mhdhassan99 <mhassan.work99@gmail.com>
2020-07-20 14:35:44 -0400

    stuck

--- pokemon-teams-frontend/src/index.js
+++ pokemon-teams-frontend/src/index.js
@@ -1,3 +1,14 @@
-const BASE_URL = "http://localhost:3000"
-const TRAINERS_URL = `${BASE_URL}/trainers`
-const POKEMONS_URL = `${BASE_URL}/pokemons`
+document.addEventListener('DOMContentLoaded', (e) => {
+    const BASE_URL = "http://localhost:3000";
+    const TRAINERS_URL = `${BASE_URL}/trainers`;
+    const POKEMONS_URL = `${BASE_URL}/pokemons`;
+
+    const fetchData = () => {
+        fetch(TRAINERS_URL)
+        .then(response => response.json())
+        .then(data => console.log(data))
+    };
+
+
+    fetchData();
+});
